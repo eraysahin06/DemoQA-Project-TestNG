d8d0fd23a03cd6c47ffa1ae3408911d0092af6d2
ragipbaylar <ragipbaylar86@gmail.com>
2023-02-24 21:48:42 -0500

    TestNG Day1

--- src/test/java/day01/_01_Intro.java
+++ src/test/java/day01/_01_Intro.java
@@ -1,8 +1,5 @@
 package day01;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class _01_Intro {
