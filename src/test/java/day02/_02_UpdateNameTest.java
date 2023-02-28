442c8b8c0974ed960356291f3d46c6d3352b397f
ragipbaylar <ragipbaylar86@gmail.com>
2023-02-27 19:51:20 -0500

    TestNG Day2 first commit

---
+++ src/test/java/day02/_02_UpdateNameTest.java
@@ -0,0 +1,12 @@
+package day02;
+
+public class _02_UpdateNameTest {
+    /** Go to "https://opencart.abstracta.us/index.php?route=account/login"
+     * Login
+     * Click on Register
+     * Fill int he form with valid informations
+     * Accept the agreement
+     * Click on Continue button
+     * Verify that you are signed up
+     */
+}
